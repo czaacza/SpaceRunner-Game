8b262735bab5cbbacbbb5f0f393320a728a8006d
Mati Czarnecki <maticzacza@gmail.com>
2022-05-14 19:26:08 +0200

    logolabel in progress

--- main/java/model/LogoLabel.java
+++ main/java/model/LogoLabel.java
@@ -6,11 +6,20 @@ public class LogoLabel extends SpaceRunnerLabel {
 
     private final int PREF_HEIGHT = 64;
     private final int PREF_WIDTH = 550;
-    private final int FONT_SIZE = 58;
+    private int FONT_SIZE = 58;
 
 
     public LogoLabel(String text) {
         super(text);
+        setLabelFont(FONT_SIZE);
+        setTextFill(Paint.valueOf("#EEEEEE"));
+        setPrefHeight(PREF_HEIGHT);
+        setPrefWidth(PREF_WIDTH);
+    }
+
+    public LogoLabel(String text, int FONT_SIZE) {
+        super(text);
+        this.FONT_SIZE = FONT_SIZE;
         setLabelFont(FONT_SIZE);
         setTextFill(Paint.valueOf("#EEEEEE"));
         setPrefHeight(PREF_HEIGHT);
--- main/java/view/ViewManager.java
+++ main/java/view/ViewManager.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import model.LogoLabel;
 import model.SHIP;
 import model.ShipPicker;
 
@@ -13,19 +14,20 @@ public class ViewManager {
     public static final int HEIGHT = 768;
     public static final int WIDTH = 1024;
 
-    private  AnchorPane mainPane;
-    private  Scene mainScene;
-    private  Stage mainStage;
-    private  ButtonManager buttonManager;
-    private  SubSceneManager subSceneManager;
+    private AnchorPane mainPane;
+    private Scene mainScene;
+    private Stage mainStage;
+    private ButtonManager buttonManager;
+    private SubSceneManager subSceneManager;
+    private LogoLabel logoLabel;
 
     private final static int MENU_BUTTONS_START_X = 100;
     private final static int MENU_BUTTONS_START_Y = 150;
 
-    private List <ShipPicker> shipsList;
+    private List<ShipPicker> shipsList;
     private SHIP chosenShip;
 
-    public ViewManager(){
+    public ViewManager() {
         mainStage = new Stage();
         mainPane = new AnchorPane();
         mainScene = new Scene(mainPane, WIDTH, HEIGHT);
@@ -39,16 +41,23 @@ public class ViewManager {
         mainStage.setTitle("Space Runner");
         mainStage.setScene(mainScene);
         createBackground();
-
+        createLogoLabel();
         subSceneManager.createSubScenes();
         buttonManager.createMainButtons();
 
     }
 
-    private void createBackground(){
-        Image image = new Image("file:src/main/resources/view/blue.png", 256,256, false, true);
+    private void createBackground() {
+        Image image = new Image("file:src/main/resources/view/blue.png", 256, 256, false, true);
         BackgroundImage background = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
         mainPane.setBackground(new Background(background));
+    }
+
+    private void createLogoLabel(){
+        logoLabel = new LogoLabel("SPACE RUNNER", 48);
+        logoLabel.setLayoutX(380);
+        logoLabel.setLayoutY(50);
+        mainPane.getChildren().add(logoLabel);
     }
 
     public Stage getMainStage() {
@@ -58,6 +67,5 @@ public class ViewManager {
     public AnchorPane getMainPane() {
         return mainPane;
     }
-
 
 }
